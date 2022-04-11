a3154fd99d41771305b048cc6ec8eeca5f2f2180
unknown <cat.crusader31@gmail.com>
2022-04-11 18:13:09 +0300

    feat: add clickable buttons
    Add clickable buttons on extension popup
    Add some regions and remove some debug stuff
    Add another permissions to manifest

--- blocker.js
+++ blocker.js
@@ -1,22 +1,48 @@
-//alert('you want to visit forbidden website!')
+
 const key ="websiteslist";
 var blockedPagesArray;
 var curPage = window.location.hostname;
+let addUrlButton    = document.getElementById("add");
+let removeUrlButton = document.getElementById("remove");
+let UrlInput = document.getElementById("url");
 
-
-
-
-
+console.log("console started");
 ShowLocalStorage();
+
+//#region ButtonListeners
+if(addUrlButton){//          add button
+addUrlButton.addEventListener("click",async()=>{
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: AddUrlButtonClicked,
+    });
+});
+}
+if(removeUrlButton){//      remove button
+    removeUrlButton.addEventListener("click",async()=>{
+        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          function: RemoveUrlButtonClicked,
+        });
+    });
+}
+//#endregion
+function AddUrlButtonClicked(){
+    
+}
+function RemoveUrlButtonClicked(){
+    
+}
 
 function removeElementFromArray(element){
     return blockedPagesArray.filter(function(value){
         return value != element;
     });
 }
-
-
-
 //#region LocalStorage Methods
 function AddToLocalStorage(url){
     blockedPagesArray = GetLocalStorage(key);
@@ -59,4 +85,3 @@ function ReplaceContent(url){
     document.head.innerHTML = generateStyle();
     document.body.innerHTML = generateHTML(url);
 }
-//corrected some typos
